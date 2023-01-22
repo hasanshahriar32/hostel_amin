31da02dc018b20ce414076ce814add4812def4ea
Mr. Hasan <hasanshahriar32@gmail.com>
2023-01-23 03:41:57 +0600

    bazar updated

--- src/Components/Main/Bazar/Bazar.js
+++ src/Components/Main/Bazar/Bazar.js
@@ -128,17 +128,17 @@ export default function Bazar() {
 
   const columns = [
     { field: 'name', headerName: 'Name', width: 180, editable: true },
-    { field: 'age', headerName: 'Age', type: 'number', editable: true },
+    { field: 'age', headerName: 'Budget', type: 'number', editable: true },
     {
       field: 'dateCreated',
-      headerName: 'Date Created',
+      headerName: 'Assigned Date',
       type: 'date',
       width: 180,
       editable: true,
     },
     {
       field: 'lastLogin',
-      headerName: 'Last Login',
+      headerName: 'Last Bazar',
       type: 'dateTime',
       width: 220,
       editable: true,
