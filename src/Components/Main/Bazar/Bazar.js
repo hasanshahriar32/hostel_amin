3abc9fbbff3ae7a1a0b62f522934b664d006a71a
Mr. Hasan <hasanshahriar32@gmail.com>
2023-01-27 07:30:20 +0600

    bazar is updating

--- src/Components/Main/Bazar/Bazar.js
+++ src/Components/Main/Bazar/Bazar.js
@@ -7,6 +7,9 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
+// import { orderDataSource } from "./data";
+ 
+
 import {
   GridRowModes,
   DataGridPro,
@@ -19,51 +22,51 @@ import {
   randomUpdatedDate,
   randomId,
 } from '@mui/x-data-grid-generator';
+import { DataGrid } from '@mui/x-data-grid';
 
-const initialRows = [
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 25,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate(),
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 36,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate(),
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 19,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate(),
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 28,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate(),
-  },
-  {
-    id: randomId(),
-    name: randomTraderName(),
-    age: 23,
-    dateCreated: randomCreatedDate(),
-    lastLogin: randomUpdatedDate(),
-  },
-];
-
+// const initialRows = [
+//   {
+//     id: randomId(),
+//     name: randomTraderName(),
+//     age: 25,
+//     dateCreated: randomCreatedDate(),
+//     lastLogin: randomUpdatedDate(),
+//   },
+ 
+  
+// ];
+// console.log(initialRows);
+// console.log(orderDataSource[0]);
 function EditToolbar(props) {
   const { setRows, setRowModesModel } = props;
 
   const handleClick = () => {
-    const id = randomId();
-    setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
+    const DatabaseId = fetch("https://amin-mess-server.vercel.app/bazar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "",
+        budget: "",
+        dateCreated: "",
+        dateFinished: "",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const id = data.insertedId;
+        return id;
+      });
+      console.log(DatabaseId);
+
+    const id = DatabaseId;
+
+    setRows((oldRows) => [
+      ...oldRows,
+      { id, name: "", budget: "", dateCreated: "", dateFinished : ""},
+    ]);
     setRowModesModel((oldModel) => ({
       ...oldModel,
       [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
@@ -85,7 +88,29 @@ EditToolbar.propTypes = {
 };
 
 export default function Bazar() {
-  const [rows, setRows] = React.useState(initialRows);
+  const [loading, setLoading] = React.useState(true);
+  const [rows, setRows] = React.useState([]);
+  const [bazarData, setBazarData] = React.useState([]);
+  React.useEffect(() => {
+     fetch('https://amin-mess-server.vercel.app/bazar')
+
+      .then((res) => res.json())  
+      .then((data) => {
+        console.log(data);
+        //destructure data and set _id to id
+        const mybazarData = data.map((bazar) => {
+          const { _id, ...rest } = bazar;
+          return { id: _id, ...rest };
+        });
+        // console.log(mybazarData);
+        setRows(mybazarData);
+// setRows(bazarData);
+        setBazarData(data);
+        setLoading(false);
+      });
+  }, []);
+  // const initialRows = bazarData;
+
   const [rowModesModel, setRowModesModel] = React.useState({});
 
   const handleRowEditStart = (params, event) => {
@@ -102,10 +127,45 @@ export default function Bazar() {
 
   const handleSaveClick = (id) => () => {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    //get renderCell params  and set it to a variable
+    const row = rows.find((row) => row.id === id);
+    console.log(row);
+    // console.log(row.name);
+
+    fetch(`https://amin-mess-server.vercel.app/bazar/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "",
+        budget: "",
+        dateCreated: "",
+        dateFinished: "",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+    console.log(id);
+    console.log(rowModesModel);
   };
 
   const handleDeleteClick = (id) => () => {
     setRows(rows.filter((row) => row.id !== id));
+    console.log(id);
+    fetch(`https://amin-mess-server.vercel.app/bazar/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      }
+      );
+
+      
+
   };
 
   const handleCancelClick = (id) => () => {
@@ -127,31 +187,119 @@ export default function Bazar() {
   };
 
   const columns = [
-    { field: 'name', headerName: 'Name', width: 180, editable: true },
-    { field: 'age', headerName: 'Budget', type: 'number', editable: true },
+    // { field: 'name', headerName: 'Name', width: 180, editable: true },
+    // { field: 'budget', headerName: 'Budget', type: 'number', editable: true },
+    // {
+    //   field: 'dateCreated',
+    //   headerName: 'Assigned Date',
+    //   type: 'date',
+    //   width: 180,
+    //   editable: true,
+    // },
+    // {
+    //   field: 'dateFinished',
+    //   headerName: 'Finish Time',
+    //   type: 'dateTime',
+    //   width: 220,
+    //   editable: true,
+    // },
+    // {
+    //   field: 'actions',
+    //   type: 'actions',
+    //   headerName: 'Actions',
+    //   width: 100,
+    //   cellClassName: 'actions',
+    //   getActions: ({ id }) => {
+    //     const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+    //     if (isInEditMode) {
+    //       return [
+    //         <GridActionsCellItem
+    //           icon={<SaveIcon />}
+    //           label="Save"
+    //           onClick={handleSaveClick(id)}
+    //         />,
+    //         <GridActionsCellItem
+    //           icon={<CancelIcon />}
+    //           label="Cancel"
+    //           className="textPrimary"
+    //           onClick={handleCancelClick(id)}
+    //           color="inherit"
+    //         />,
+    //       ];
+    //     }
+
+    //     return [
+    //       <GridActionsCellItem
+    //         icon={<EditIcon />}
+    //         label="Edit"
+    //         className="textPrimary"
+    //         onClick={handleEditClick(id)}
+    //         color="inherit"
+    //       />,
+    //       <GridActionsCellItem
+    //         icon={<DeleteIcon />}
+    //         label="Delete"
+    //         onClick={handleDeleteClick(id)}
+    //         color="inherit"
+    //       />,
+    //     ];
+    //   },
+    // },
+// save data of field to a variable
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 180,
+      editable: true,
+      renderCell: (params) => {
+        const { value } = params;
+        return <span>{value}</span>;
+      },
+    },
+    {
+      field: 'budget',
+      headerName: 'Budget',
+      type: 'number',
+      editable: true,
+      renderCell: (params) => {
+        const { value } = params;
+        return <span>{value}</span>;
+      },
+    },
     {
       field: 'dateCreated',
       headerName: 'Assigned Date',
       type: 'date',
       width: 180,
       editable: true,
+      renderCell: (params) => {
+        const { value } = params;
+        return <span>{value}</span>;
+      },
     },
     {
-      field: 'lastLogin',
-      headerName: 'Last Bazar',
+      field: 'dateFinished',
+      headerName: 'Finish Time',
       type: 'dateTime',
       width: 220,
       editable: true,
+      renderCell: (params) => {
+        const { value } = params;
+        return <span>{value}</span>;
+      },
     },
     {
       field: 'actions',
       type: 'actions',
       headerName: 'Actions',
       width: 100,
+
       cellClassName: 'actions',
       getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
 
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+    
         if (isInEditMode) {
           return [
             <GridActionsCellItem
@@ -186,8 +334,14 @@ export default function Bazar() {
         ];
       },
     },
+
+
   ];
 
+
+
+  
+    
   return (
     <Box
     className="bg-white"
@@ -202,9 +356,12 @@ export default function Bazar() {
         },
       }}
     >
-      <DataGridPro
+      <DataGrid
         rows={rows}
         columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[10 , 20, 50, 100]}
+// height={500}
         editMode="row"
         rowModesModel={rowModesModel}
         onRowModesModelChange={(newModel) => setRowModesModel(newModel)}
