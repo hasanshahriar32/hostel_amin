103f3637aa748cf1ad1a102f1d5ecd257c5bce11
Mr. Hasan <hasanshahriar32@gmail.com>
2023-01-27 19:26:30 +0600

    index icon and title updated

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -8,7 +8,16 @@ const Main = () => {
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
-      
+      <div
+        data-tf-popover="B9mOgUOl"
+        data-tf-custom-icon="https://avatars.githubusercontent.com/u/109535601?v=4"
+        data-tf-button-color="#F0E5C1"
+        data-tf-button-text="Launch me"
+        data-tf-iframe-props="title=Product Review Form Template (copy)"
+        data-tf-medium="snippet"
+        // style="all:unset;"
+      ></div>
+      <script src="//embed.typeform.com/next/embed.js"></script>
       <Footer></Footer>
     </div>
   );
